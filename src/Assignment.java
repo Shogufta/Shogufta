1637a452651d1faa2ab2dee257afd96e3fe81780
ShHal <sh.hallimee@gmail.com>
2024-08-08 20:30:53 -0400

    My first assignment

---
+++ src/Assignment.java
@@ -0,0 +1,9 @@
+public class Assignment {
+    public static void main(String[] arg) {
+
+
+        System.out.println("My first project");
+        System.out.println("I'm learning java");
+        System.out.println("Hello Everyone");
+    }
+}
