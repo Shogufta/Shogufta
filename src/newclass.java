d5278b4b6ce18c24f20c46633bde6766519d63c0
ShHal <sh.hallimee@gmail.com>
2024-08-08 20:48:47 -0400

    New project

---
+++ src/newclass.java
@@ -0,0 +1,9 @@
+public class newclass {
+    public static void main(String[] arg) {
+
+
+        System.out.println("My first project");
+        System.out.println("I'm learning java");
+        System.out.println("Hello Everyone");
+    }
+}
